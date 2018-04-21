9d19b79b0bcf22bc513bb9651bd8e7c8d3416c37
Fabian Beuke <mail@beuke.org>
2018-04-21 23:15:03 +0200

    add deploy script

---
+++ gulpfile.babel.js
@@ -0,0 +1,11 @@
+import gulp from 'gulp'
+import htmlmin from 'gulp-htmlmin'
+import ghPages from 'gulp-gh-pages'
+
+gulp.task('deploy', () => gulp.src('./public/**/*').pipe(ghPages()) )
+
+gulp.task('minify', () => {
+    return gulp.src('./public/**/*.html')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('public'))
+})
